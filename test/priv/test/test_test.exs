3fd10aabf577e1a9f9c5e272b8031409ad2204af
Frank Hunleth <fhunleth@troodon-software.com>
2018-06-18 09:00:13 -0400

    Add tests

--- test/priv/test/test_test.exs
+++ test/priv/test/test_test.exs
@@ -6,4 +6,12 @@ defmodule TestTest do
     assert File.exists?("/dev/i2c-0")
     assert File.exists?("/dev/i2c-2")
   end
+
+  test "the virtual serial port ttyGS0 exists" do
+    assert File.exists?("/dev/ttyGS0")
+  end
+
+  test "/boot is mounted" do
+    assert File.exists?("/boot/MLO")
+  end
 end
